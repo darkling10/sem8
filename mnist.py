82759ab887c9561a4fba67fb889a4bcbf89b21ef
Abbas Pathan <pathan.abbas10@gmail.com>
2024-04-23 21:56:50 +0530

    Add mnsit

---
+++ mnist.py
@@ -0,0 +1,75 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+fashion_mnist = tf.keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+train_images.shape
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+# Verify data is in correct format
+plt.figure(figsize = (10, 10))
+for i in range(25):
+  plt.subplot(5, 5, i + 1)
+  plt.xticks([])
+  plt.yticks([])
+  plt.grid(False)
+  plt.imshow(train_images[i], cmap = plt.cm.binary)
+  plt.xlabel(class_names[train_labels[i]])
+plt.show
+
+from keras.models import Sequential
+from keras.layers import Dense
+
+model = Sequential()
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape = (28, 28)),
+    tf.keras.layers.Dense(128, activation = 'relu'),
+    tf.keras.layers.Dense(10)
+])
+
+model.compile(optimizer = 'adam', loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits = True), metrics = ['accuracy'])
+model.summary()
+
+model.fit(train_images, train_labels, epochs = 10)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose = 2)
+print("\n Test accuracy = ", test_acc)
+
+probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
+
+predictions = probability_model.predict(test_images)
+
+def plot_image(i, predictions_array, true_label, img):
+  true_label, img = true_label[i], img[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+
+  plt.imshow(img, cmap=plt.cm.binary)
+
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
+
+  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
+
+rows = 5
+cols = 5
+total_images = rows * cols
+plt.figure(figsize = (2*2*cols, 2*rows))
+for i in range(total_images):
+  plt.subplot(rows, cols, i + 1)
+  plot_image(i, predictions[i], test_labels, test_images)
+plt.tight_layout()
+plt.show()
